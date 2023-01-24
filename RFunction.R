d128133339a7ef65f602b05cdb0303434ee8e81e
andreakoelzsch <akoelzsch@ab.mpg.de>
2023-01-24 14:34:21 +0100

    adapt RFunction.R
    add lines to force list even if one track telemetry object

--- RFunction.R
+++ RFunction.R
@@ -3,5 +3,7 @@ library('ctmm')
 
 # input: moveStack - output: telemetry.list
 rFunction <- function(data) {
-  as.telemetry(data)
+  result <- as.telemetry(data)
+  if (is(result,'telemetry')) result <- list(result) #force list, can be with emtpy table
+  return(result)
 }
